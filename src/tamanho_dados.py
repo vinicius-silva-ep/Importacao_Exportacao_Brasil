cac73b7b37d079988814a42ccebcddfca433677d
viniscera <vinicius.silva.ep@outlook.com>
2023-12-03 00:12:53 -0300

    transformações do dw

---
+++ src/tamanho_dados.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+caminho_arquivo = 'C:\Users\vinic\Documents\ESTUDOS_DATA_SCIENCE\PORTFÓLIO\IMPORTAÇÃO E EXPORTAÇÃO DO BRASIL DE 1996 A 2023\EXP_COMPLETA\EXP_COMPLETA.csv'
+
+dados = pd.read_csv(caminho_arquivo)
